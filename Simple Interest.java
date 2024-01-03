d45594cb19c414d1d79c05f26258e1769b4354db
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-03 23:18:28 +0530

    Update Simple Interest.java

--- Simple Interest.java
+++ Simple Interest.java
@@ -1,1 +1,19 @@
 
+import java.util.Scanner;
+public class simpleinterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Principal");
+        int p = sc.nextInt();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Time");
+        int t = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your rate");
+        int r = scanner.nextInt();
+        int d;
+        d= (p * r * t)/100;
+        System.out.println("Simple Interest is Rs. "+d);
+
+    }
+}

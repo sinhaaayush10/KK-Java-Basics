5fdf13b679573370f181b631e61237a14c585c5e
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-03 20:23:53 +0530

    Greetings for user name

---
+++ Q2.A Greeting message for user input Name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class greetings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String a = sc.nextLine();
+        System.out.print("Hi " +a );
+        System.out.print(" Hope Your Day Goes well");
+    }
+}

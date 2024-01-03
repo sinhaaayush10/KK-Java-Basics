a8aa48639815a695fa41f8d827971a0fa6fe716d
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-04 00:04:16 +0530

    Update Q6.Currency Conversion.java

--- Q6.Currency Conversion.java
+++ Q6.Currency Conversion.java
@@ -1,1 +1,11 @@
 
+import java.util.Scanner;
+public class currencyconversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Currency in INR");
+        float a = sc.nextFloat();
+        float b = a/83.29f;
+        System.out.println("Currency in USD "+b);
+    }
+}

7c5fbe633a8b859946f4a96ef1989f3ee7e1e18d
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-03 23:54:42 +0530

    Update Q5.print the largest no.java

--- Q5.print the largest no.java
+++ Q5.print the largest no.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class largestno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st No");
+        int a = sc.nextInt();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 2nd No");
+        int b = scan.nextInt();
+        if (a>b)
+            System.out.println("1st no is greater than 2nd no");
+        else
+            System.out.println("2nd no is greater than 1st no");
+        
+
+    }
+}
 

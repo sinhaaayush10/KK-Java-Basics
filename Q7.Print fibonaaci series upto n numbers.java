cfda034843b479977a82197dbadcdd31c076c956
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-05 14:34:15 +0530

    Print fibo series upto n nos

--- Q7.Print fibonaaci series upto n numbers.java
+++ Q7.Print fibonaaci series upto n numbers.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;
+public class fibonaaci {
+    public static void main(String[] args) {
+        int term,a=0,b=1,c;
+        System.out.println("Enter No of terms");
+        Scanner sc = new Scanner(System.in);
+         term = sc.nextInt();
+        for (int i=0;i<=term;i++) {
+            System.out.print(a + " ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
+
+    }
+}

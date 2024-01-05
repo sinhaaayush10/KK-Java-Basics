167e22926bd9435dc4cc00fe5c42cdc2c36db342
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-05 21:50:39 +0530

    Print of tribonacci series upto n nos

--- Q8.Print tribonnaci seires upto n number.java
+++ Q8.Print tribonnaci seires upto n number.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+public class tribonacci {
+    public static void main(String[] args) {
+        int term,a=0,b=1,c=2,d;
+        System.out.println("Enter no of terms");
+        Scanner sc =new Scanner(System.in);
+        term = sc.nextInt();
+        for (int i = 1;i<=term;i++)
+        {
+            System.out.print(a+ " ");
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+    }
+}

969881f78e5eed7b3d1dd033e13f18db91fb3353
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-06 18:50:53 +0530

    Armstrong no. eg 1,153,370,371,407 etc

--- Q9.Check whether the number is armstrong or not.java
+++ Q9.Check whether the number is armstrong or not.java
@@ -1,1 +1,21 @@
 
+import java.util.Scanner;
+public class armstrong {
+    public static void main(String[] args) {
+        int n,arm=0,rem,c;
+        System.out.print("Enter the no: ");
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+        c=n;
+        while(n>0)
+        {
+            rem = n%10;
+            arm = (rem*rem*rem)+ arm;
+            n=n/10;
+        }
+        if(c==arm)
+        System.out.println("The no is Armstrong");
+        else
+        System.out.println("The no is not Armstrong");
+    }
+}

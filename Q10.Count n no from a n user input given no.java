bd51cbccfe3d859dbff8e3710e6fc89424863222
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-09 23:47:19 +0530

    eg. 1223423// Ans: 3

--- Q10.Count n no from a n user input given no.java
+++ Q10.Count n no from a n user input given no.java
@@ -1,1 +1,17 @@
 
+public class countnos {
+
+    public static void main(String[] args) {
+        int n = 13435378;
+
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 3) {
+                count++;
+            }
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}

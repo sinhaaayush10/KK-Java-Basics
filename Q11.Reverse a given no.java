cd1e99764bbc01b54093bc65051974e15ceb284f
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-09 23:48:51 +0530

    Eg. 1234 // Ans. 4321

--- Q11.Reverse a given no.java
+++ Q11.Reverse a given no.java
@@ -1,1 +1,16 @@
 
+import java.util.Scanner;
+public class reversee {
+    public static void main(String[] args) {
+        System.out.println("Enter a No.");
+        Scanner r = new Scanner(System.in);
+        int  n = r.nextInt();
+        int ans = 0;
+        while(n > 0){
+            int rem = n %10;
+            n /= 10;
+            ans = ans * 10  + rem;
+        }
+        System.out.println(ans);
+    }
+}

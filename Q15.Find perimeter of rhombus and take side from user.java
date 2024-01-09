b6510b159aaf13d77268150e13798173224c152c
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-10 00:29:28 +0530

    Perimeter of rhombus = 4a

--- Q15.Find perimeter of rhombus and take side from user.java
+++ Q15.Find perimeter of rhombus and take side from user.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+public class perimeterhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Side: ");
+        float side = sc.nextFloat();
+
+        float area = 4*side;
+
+        System.out.println("Area of rhombus is "+area);
+    }
+}
 

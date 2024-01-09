7a8413eee2d98d700d32a465af1cd3d584ea55b3
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-10 00:38:59 +0530

    CSA of Cylinder = 2*3.14*r*h

--- Q16.Find CSA of Cylinder.java
+++ Q16.Find CSA of Cylinder.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class csacylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Radius(r): ");
+        float r = sc.nextFloat();
+        System.out.println("Enter The Height(h): ");
+        float h = sc.nextFloat();
+
+        float area = 2 * 3.14f * r*h;
+
+        System.out.print("The CSA of Cylinder is "+area);
+        System.out.println("cm sq.");
+
+    }
+}
 

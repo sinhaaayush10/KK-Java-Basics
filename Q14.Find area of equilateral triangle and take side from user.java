efd627c4088f3a34b74dbac8841b73329c56ec30
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-10 00:24:03 +0530

    Area of equilateral triangle = 1.73/4 * (side*side)

--- Q14.Find area of equilateral triangle and take side from user.java
+++ Q14.Find area of equilateral triangle and take side from user.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+public class areaequilateraltriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Side : ");
+        float side = sc.nextFloat();
+        float area = 1.73f/4 * side*side;
+        System.out.println("The area of equilateral triangle is "+area);
 
+    }
+}

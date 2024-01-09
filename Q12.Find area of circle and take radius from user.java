d6229d8268e3426d6c9ca519e1c47e9159a88029
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-09 23:58:47 +0530

    Q12.Find area of circle and take radius from user.java

--- Q12.Find area of circle and take radius from user.java
+++ Q12.Find area of circle and take radius from user.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class areacircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Radius");
+        float n = sc.nextInt();
+        float area = 3.14f*n*n;
+        System.out.println("Area Of Circle is "+area);
+    }
+}

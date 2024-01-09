8b5898504211ebfb9c6c7b964ea0823580f19fec
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-10 00:06:02 +0530

    Area Of Trianlge = (b*h)/2

--- Q13.Find area of triangle and b and h from user.java
+++ Q13.Find area of triangle and b and h from user.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class areatriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Base : ");
+        float b = sc.nextFloat();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Height : ");
+        float h = scan.nextFloat();
 
+        float area = (b*h)/2f;
+        System.out.println("Area Of Triangle is "+area);
+    }
+}

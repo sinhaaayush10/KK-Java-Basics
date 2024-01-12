f209b88fd66e3138c4b74ada4c235b6997ba27d0
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-12 16:05:49 +0530

    Input:- 5 // Output:- 120

--- Q18.Find Factorial Of a user given user input.java
+++ Q18.Find Factorial Of a user given user input.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Factorial {
+    public static void main(String[] args) {
+        int fact =1;
+        System.out.print("Enter a no: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1;i<=num; i++){
+
+            fact = fact*i;
+        }
+        System.out.print("The Factorial of "+num);
+        System.out.println(" is "+fact);
+    }
+}

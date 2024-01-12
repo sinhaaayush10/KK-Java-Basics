358ae903ed54a8034978fac460f86a8aa6a641e3
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-13 01:01:26 +0530

    Input:18 , 4 // output: 22

--- Q20.Sum 2 number using function.java
+++ Q20.Sum 2 number using function.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class Sumwithfun {
+    public static void main(String[] args) {
+        int ans = sum2();
+        System.out.println("The sum of 2 nos is: "+ans);
+    }
+    static int sum2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No 1: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter no 2: ");
+        int num2 = sc.nextInt();
 
+        int Sumwithfun = num1 + num2;
+        return Sumwithfun;
+    }
+}

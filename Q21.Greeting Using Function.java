c79718aacbd68e460ba9a4f976bbccbea95e98c2
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-13 01:33:25 +0530

    Function used in greetings

--- Q21.Greeting Using Function.java
+++ Q21.Greeting Using Function.java
@@ -1,1 +1,11 @@
+public class Stringeg {
 
+    public static void main(String[] args) {
+        String personalised = myGreet("Aayush Sinha");
+        System.out.println(personalised);
+    }
+    static String myGreet(String name){
+        String message = "Kemon Aacho " +name;
+        return message;
+    }
+}

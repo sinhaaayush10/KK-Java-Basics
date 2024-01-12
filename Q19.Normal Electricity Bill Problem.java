60008d32aca4fb90c5b929b7b973a9bfc4a3d4d3
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-12 16:17:05 +0530

    Input: 45 // output: 315

--- Q19.Normal Electricity Bill Problem.java
+++ Q19.Normal Electricity Bill Problem.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Electricitybill {
+    public static void main(String[] args) {
+        System.out.print("Enter The Consumed Units: ");
+        Scanner sc = new Scanner(System.in);
+        float unit = sc.nextFloat();
+
+        float total = 7*unit;
+
+        System.out.print("The Consumed Unit "+unit);
+        System.out.println(" And total amount is "+total);
+    }
+}
 

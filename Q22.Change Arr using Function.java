e6ff4d165426f5d27b999742a55c38ff14e29267
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-01-13 02:14:30 +0530

    Given Arr = {1,3,4,18,20}  /// Resultant Arr = {1,45,4,18,20}

--- Q22.Change Arr using Function.java
+++ Q22.Change Arr using Function.java
@@ -1,1 +1,11 @@
-
+import java.util.Arrays;
+public class Arr {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,18,20};
+        change(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+    static void change(int[] nums){
+        nums[1] = 45;
+    }
+}

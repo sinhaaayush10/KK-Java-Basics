0793a3230a29efc6be5c7a6acdd9fd3b41da9d3d
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-13 13:30:38 +0530

    Input: 350 /// output: yes

---
+++ Q35.The user given no is int or not.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class CWH_IntOrNot{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.hasNext());
+
+    }
+}

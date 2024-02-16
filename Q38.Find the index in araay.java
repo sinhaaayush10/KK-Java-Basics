a4304e0eb9d0199a4a2e457f6fb394f3f157241f
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-16 23:10:02 +0530

    Input: {1,3,4,5,6,} /// Found=4 ///Output: 2

--- Q38.Find the index in araay.java
+++ Q38.Find the index in araay.java
@@ -1,1 +1,28 @@
 
+import java.util.Scanner;
+public class SD_Index {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of arr:");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        System.out.print("Enter the numbers: ");
+
+        int numbers[] = new int[size];
+//        for input:
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        System.out.print("enter no to found:");
+
+        int x = sc.nextInt();
+//        for output:
+
+        for (int i = 0; i < numbers.length; i++) {
+                       if (numbers[i] == x){
+               System.out.print("x found at index: "+i);
+           }
+        }
+    }
+
+}
+

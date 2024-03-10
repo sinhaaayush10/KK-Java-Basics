8874bc2bf1759b6de540af0c6ba0cac356e89f13
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-03-10 18:56:10 +0530

    Right angle triangle pattern

---
+++ Q46.Pattern_2.java
@@ -0,0 +1,15 @@
+public class pattern_2 {
+    public static void main(String[] args) {
+        int i=0;
+        int j=0;
+        for (i=0; i<5; i++)
+        {
+            for(j=0; j<=i; j++)
+                System.out.print("* ");
+                System.out.println(" ");
+
+
+        }
+        System.out.println(" ");
+    }
+}

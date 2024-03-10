c8ae016f23703d54a6d3ffd6e8e30b1d2e0fd6c3
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-03-10 18:48:27 +0530

    Simple square pattern

---
+++ Pattern_1.java
@@ -0,0 +1,14 @@
+public class pattern_1 {
+    public static void main(String[] args) {
+        int i =0;
+        int j= 0;
+        for (i=0; i<4; i++)
+        {
+            for (j=0; j<4; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        System.out.println(" ");
+    }
+}
+ 
